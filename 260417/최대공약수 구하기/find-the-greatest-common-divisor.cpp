87820aa8fbc48ff72218d1a5f8ee7ca2b4390116
ndieeeee <105354216+jude-z@users.noreply.github.com>
2026-04-17 21:37:37 +0900

    Title: 최대공약수 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 260417/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -6,7 +6,7 @@ int main() {
     int n, m;
     cin >> n >> m;
     int min_val = min(n,m);
-    int max_val = -1;
+    int max_val = 1;
     for(int i= 2;i<=min_val;i++){
         if(n % i == 0 && m % i == 0) max_val = max(max_val,i);
     }
